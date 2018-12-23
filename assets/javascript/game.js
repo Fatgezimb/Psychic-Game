0f6acab7439d2d4103671db6939b2ec335988c27
fatgezimb <fatgezimbela1@gmail.com>
2018-12-22 21:07:26 -0500

    Final Comit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,29 +5,9 @@ var losses = 0;
 var guessesLeft = 5;
 var computerChoice;
 
+
 // List for the guess that the user does. These need to be printed on the page
-var yourGuess = [];
-
-// computers guess to appear when user loses or wins
-
-// ======================================================================
-// LOOK HERE TO FINISH THIS PART
-var computerGuess = document.getElementById("computerGuess");
-
-// ======================================================================
-// score wins losses
-var userWins = document.getElementById("userWins");
-var userLosses = document.getElementById("userLosses");
-
-// ======================================================================
-// user guesses left
-var userGuessesLeft = document.getElementById("userGuessesLeft");
-
-// ======================================================================
-// display users guesses
-var userGuesses = document.getElementById("userGuesses");
-
-
+var previousGuess = [];
 
 
 // ======================================================================
@@ -36,70 +16,62 @@ game();
 
 function game() {
     // computer needs to pick random letter
-    var computerChoice = ('abcdefghijklmnopqrstuvwxyz').split('')[(Math.floor(Math.random() * 26 ))];
-    console.log(computerChoice);
+    var computerChoice = ('abcdefghijklmnopqrstuvwxyz').split('')[(Math.floor(Math.random() * 26))];
+    console.log("Computer Choice: " + computerChoice);
 
     // initiates the below function
     compareGuess();
-
-
-// ======================================================================
+    // ======================================================================
     // correct guess funtion
     // Will determine if the letter guessed matched the computers guess
     function compareGuess() {
-        
-       
-    //    Not working code :(
-        // // get user guess
-        // document.onkeyup = function (keyPress) {
-        //     var yourGuess = String.keyPress.key
-        //     console.log("This is your guess: " +yourGuess)
-        // }
-// ======================================================================
-        document.onkeyup = function(event) {
-
+        // ======================================================================
+        // ======================================================================
+        document.onkeyup = function (event) {
             // Determines which key was pressed.
-            var yourGuess = event.key;
-            console.log("your guess: "+yourGuess);
-            console.log("_______________________" )
-        // Correct need to make wins go up by one
+            var userGuess = event.key;
+            letter = userGuess
+            console.log("Letter: " + letter);
 
-// ======================================================================
+            // console.log("your guess: " + userGuess);
+            // console.log("_______________________")         
 
-        if (yourGuess == computerChoice) {
-            console.log("YOU WIN")
-            wins++;
-            console.log("Wins: "+wins )
-            console.log("losses: "+losses )
-            console.log("_______________________" )
-        } 
+            // Correct need to make wins go up by one
+            // ======================================================================
 
-// ======================================================================
-        else if (yourGuess != computerChoice) {
-            guessesLeft--;
-            console.log("Wins: "+wins )
-            console.log("gussesLeft: "+guessesLeft)
-            console.log("_______________________" )
+            if (userGuess == computerChoice) {
+                console.log("YOU WIN");
+                alert("You Win: "+ computerChoice);
+                wins++;
+                document.getElementById("userWins").innerHTML = wins;
+                resetGame();
+                // console.log("Wins: " + wins);
+                // console.log("losses: " + losses);
+                console.log("_______________________");
+            }
+            // ======================================================================
+           
+            else {
+                guessesLeft--;
+                document.getElementById("userGuessesLeft").innerHTML = guessesLeft;
 
-// ======================================================================
-            if (guessesLeft <=0) {
-                losses++
-                console.log("Wins: "+wins )
-                console.log("Losses: "+losses)
-                console.log("_______________________" )
-                // need to restart the game
+                // This addes the guess to the array userGuesses
+
+                previousGuess.push(letter);
+                console.log("TESTETTESTEETS:" + previousGuess);
+                document.getElementById("userPreviousGuesses").innerHTML = previousGuess;
+
+                // console.log("Wins: " + wins);
+                // console.log("Your Guess: " + userGuess);
+                console.log("Your Guess: " + userGuess);
+                console.log("_______________________");
+                // ======================================================================
+                document.getElementById("userGuessesLeft").innerHTML = guessesLeft;
+                outOfGuesses();
+
             }
 
-        
         }
-        
-        
-        
-        
-        
-        }
-        
-                
 
         // compares userGuess to computerGuess
 
@@ -108,12 +80,40 @@ function game() {
 
         // if the guess is incorrect do this
 
-        // print the users guesses and place them in them list yourGuess
+        // print the users guesses and place them in them list userGuesses
+    }
+
+
+
+    // reset the game and start again
+    // --------------------------------------------------------------------
+    function resetGame() {
+        
+        guessesLeft = 5;
+        previousGuess = [];
+        document.getElementById("userGuessesLeft").innerHTML = guessesLeft;
+        document.getElementById("userPreviousGuesses").innerHTML = previousGuess;
+        document.getElementById("computerChoice").innerHTML = computerChoice;
+        
+        game();
+    }
 
 
 
 
-
+    // Out of guesses for this game check
+    // --------------------------------------------------------------------
+    function outOfGuesses() {
+        if (guessesLeft === 0) {
+            console.log("Next I will take over the world");
+            alert("game over you lose");
+            losses++;
+            document.getElementById("userLosses").innerHTML = losses;
+            resetGame();
+        } else {
+            compareGuess();
+            console.log("TRY AGAIN, WRONG GUESS")
+        }
 
     }
 
@@ -121,11 +121,4 @@ function game() {
 
 
 
-
-
 }
-
-
-
-
-
